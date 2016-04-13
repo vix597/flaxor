6a5ae72a6ae9ffb09df2cc772a058ce2a2fcfbfa
Sean LaPlante <laplantes@ainfosec.com>
2016-04-13 17:26:46 -0400

    Mac addresses are 6 bytes, not 8

--- FlagDecoder/FlagDecoder/main.cpp
+++ FlagDecoder/FlagDecoder/main.cpp
@@ -11,8 +11,7 @@
 
 using namespace std;
 
-void encode(const string& path);
-void decode(const string& path);
+void encode_decode(const string& path, const string& mode);
 uint8_t * getMacAddress();
 
 int main(int argc, char ** argv) {
@@ -23,10 +22,10 @@ int main(int argc, char ** argv) {
 	}
 
 	if(!strcmp(argv[1],"encode")) {
-		encode(argv[2]);
+		encode_decode(argv[2],".encode");
 	}
 	else if (!strcmp(argv[1], "decode")) {
-		decode(argv[2]);
+		encode_decode(argv[2],".decode");
 	}
 	else {
 		cerr << "Invalid option: \"" << argv[1] << "\". Expected \"encode\" or \"decode\"\n";
@@ -36,67 +35,48 @@ int main(int argc, char ** argv) {
 	return 0;
 }
 
-void encode(const string& path) {
+void encode_decode(const string& path, const string& mode) {
 	if (!PathFileExistsA(path.c_str())) {
 		cerr << "Path: " << path << " does not exist. Cannot encode\n";
 		exit(1);
 	}
 
-	ifstream t(path,std::ios::binary);
-	string str;
+	FILE * f = NULL;
 
-	t.seekg(0, std::ios::end);
-	str.reserve(t.tellg());
-	t.seekg(0, std::ios::beg);
-	str.assign((std::istreambuf_iterator<char>(t)),std::istreambuf_iterator<char>());
-
-	int i = 0;
-	uint8_t * mac = getMacAddress();
-	for (auto& c : str) {
-		if (i == 8)
-			i = 0;
-		c = c ^ mac[i];
-		i++;
-	}
-
-	ofstream o(path + ".encode",std::ios::binary);
-	o.write(str.c_str(),str.length());
-	return;
-}
-
-void decode(const string& path) {
-	if (!PathFileExistsA(path.c_str())) {
-		cerr << "Path: " << path << " does not exist. Cannot encode\n";
+	errno_t err = fopen_s(&f, path.c_str(), "rb");
+	if (err) {
+		cerr << "Unable to open " << path << " for reading. Errno: " << err << "\n";
 		exit(1);
 	}
 
-	ifstream t(path, std::ios::binary);
-	string str;
+	fseek(f, 0L, SEEK_END);
+	uint64_t size = ftell(f);
+	uint8_t * buf = new uint8_t[size];
+	fseek(f, 0L, SEEK_SET);
 
-	t.seekg(0, std::ios::end);
-	str.reserve(t.tellg());
-	t.seekg(0, std::ios::beg);
-	str.assign((std::istreambuf_iterator<char>(t)), std::istreambuf_iterator<char>());
+	fread_s(buf, size, sizeof(uint8_t), size, f);
+	fclose(f);
 
-	int i = 0;
-	uint8_t * mac = getMacAddress();
-	for (auto& c : str) {
-		if (i == 8)
-			i = 0;
-		c = c ^ mac[i];
-		i++;
+	const uint8_t * mac = getMacAddress();
+	for (int i = 0; i < size; i++) {
+		buf[i] ^= mac[i % 6];
+	}
+	delete mac;
+
+	err = fopen_s(&f, (path + mode).c_str(), "wb");
+	if (err) {
+		cerr << "Unable to open " << path << " for writing. Errno: " << err << "\n";
+		exit(1);
 	}
 
-	ofstream o(path + ".decode", std::ios::binary);
-	o.write(str.c_str(), str.length());
+	fwrite(buf, sizeof(uint8_t), size, f);
+	fclose(f);
 	return;
 }
 
 uint8_t * getMacAddress() {
 	IP_ADAPTER_INFO *info = NULL, *pos;
 	DWORD size = 0;
-	uint8_t * mac = new uint8_t[8];
-
 	GetAdaptersInfo(info, &size);
 
 	info = (IP_ADAPTER_INFO *)malloc(size);
@@ -109,7 +89,9 @@ uint8_t * getMacAddress() {
 		for (int i = 1; i<pos->AddressLength; i++)
 			printf(":%2.2x", pos->Address[i]);
 	}
-
+	cout << "\n";
+	uint8_t * mac = new uint8_t[info->AddressLength];
+	memset(mac, 0, info->AddressLength);
 	RtlCopyMemory(mac, info->Address, info->AddressLength);
 	free(info);
 	return mac;
